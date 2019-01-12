870b8b120281dca086f925c1284c6b6169e78191
Bharat Giddwani <32789197+tarahb2103@users.noreply.github.com>
2019-01-12 11:42:33 +0530

    Add files via upload

---
+++ gpu/gpu_mnist_speedcheck.py
@@ -0,0 +1,22 @@
+import time
+
+import tensorflow as tf
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+start = time.time()
+model.fit(x_train, y_train, epochs=15)
+print('Total training time: ', time.time()-start)
+model.evaluate(x_test, y_test)
